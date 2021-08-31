d0e82e029d259e7e32d1e9b441a937b6fcc554e3
MaschavanderMarel <maschavandermarel@gmail.com>
2021-08-31 11:10:43 +0200

    remove unused code

--- app/main.py
+++ app/main.py
@@ -5,23 +5,11 @@ from mail import email
 
 class Query(ObjectType):
 
-    hello = String(name=String(default_value="stranger"))
-    goodbye = String()
     sendMessage = String(emailAddress=String(), message=String())
-
-    def resolve_hello(self, info, name):
-        # return "Hello " + name
-        return f'Hello {name}!'
-
-    def resolve_goodbye(root, info):
-        email()
-        return 'Your message was sent.'
 
     def resolve_sendMessage (root, info, emailAddress, message):
         email(emailAddress, message)
         return 'Your message was sent to CollAction'
-
-
 
 app = FastAPI()
 app.add_route("/", GraphQLApp(schema=Schema(query=Query)))
