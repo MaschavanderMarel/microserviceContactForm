cfc8c79da2dd4e91fba2cae36919a246a425b8b1
Ruben Horn <horn.ruben@yahoo.de>
2021-09-01 13:54:45 +0200

    catch error when sending email

--- app/main.py
+++ app/main.py
@@ -1,15 +1,21 @@
-from graphene import ObjectType, String, Schema
+from graphene import ObjectType, String, Schema, Field
 from fastapi import FastAPI
 from starlette.graphql import GraphQLApp
-from mail import email
+from mail import send_email
 
 class Query(ObjectType):
+    class Result(ObjectType):
+            status = String()
+            message = String()
 
-    sendMessage = String(emailAddress=String(), message=String())
+    sendMessage = Field(Result, emailAddress=String(), message=String())
 
     def resolve_sendMessage (root, info, emailAddress, message):
-        email(emailAddress, message)
-        return 'Your message was sent to CollAction'
+        try:
+            send_email(emailAddress, message)
+            return Query.Result(status='success', message='Your message was sent to CollAction')
+        except:
+            return Query.Result(status='error', message='Could not send message')
 
 app = FastAPI()
 app.add_route("/", GraphQLApp(schema=Schema(query=Query)))
